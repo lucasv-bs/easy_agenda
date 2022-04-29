2a960611a7155d566ce2511520aed474b15767fe
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-04-29 20:22:54 -0300

    Add the appointment app

--- easy_agenda/settings.py
+++ easy_agenda/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'customer.apps.CustomerConfig',
     'employee.apps.EmployeeConfig',
+    'appointment.apps.AppointmentConfig',
 ]
 
 MIDDLEWARE = [
