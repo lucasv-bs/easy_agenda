cfb742b8cfd46302baa2460bb56da4014b64a956
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-04-29 23:46:37 -0300

    Add migrations for consultation app

--- consultation/models.py
+++ consultation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import appointment
 
-# Create your models here.
+from appointment.models import Appointment
+from employee.models import Employee
+from users.models import User
+
+
+class Consultation(models.Model):
+    medical_chart = models.FileField()
+    appointment = models.ForeignKey(Appointment, on_delete=models.PROTECT)
+    doctor = models.ForeignKey(Employee, on_delete=models.PROTECT)
+    updater = models.ForeignKey(User, on_delete=models.PROTECT)    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
