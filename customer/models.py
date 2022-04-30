2b1148653c46fa8958f9db95f79e27787b1bb7aa
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-04-29 21:30:25 -0300

    Add migrations for the customer app

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.forms import CharField, DateField, DateTimeField
 
-# Create your models here.
+from users.models import User
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=1)
+    postal_code = models.CharField(max_length=8)
+    address = models.CharField(max_length=150)
+    address_number = models.CharField(max_length=7)
+    complement = models.CharField(max_length=70, null=True)
+    state = models.CharField(max_length=2)
+    city = models.CharField(max_length=150)
+    neighborhood = models.CharField(max_length=150)
+    tel1 = models.CharField(max_length=11)
+    tel2 = models.CharField(max_length=11, null=True)
+    creator = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='customer_creator_set')
+    updater = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='customer_updater_set')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
