0865010486fd5d465f859a34f8ece1fcc2cb9271
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-04-29 21:44:24 -0300

    Add migrations for employee and specialty apps

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+from users.models import User
+from specialty.models import Specialty
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11)    
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=1)
+    crm = models.CharField(max_length=13, null=True)
+    postal_code = models.CharField(max_length=8)
+    address = models.CharField(max_length=150)
+    address_number = models.CharField(max_length=7)
+    complement = models.CharField(max_length=70, null=True)
+    state = models.CharField(max_length=2)
+    city = models.CharField(max_length=150)
+    neighborhood = models.CharField(max_length=150)
+    tel1 = models.CharField(max_length=11)
+    tel2 = models.CharField(max_length=11, null=True)
+    specialty = models.ForeignKey(Specialty, null=True, on_delete=models.PROTECT)
+    creator = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='employee_creator_set')
+    updater = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='employee_updater_set')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- specialty/models.py
+++ specialty/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='specialty_creator_set')
+    updater = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='specialty_updater_set')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
