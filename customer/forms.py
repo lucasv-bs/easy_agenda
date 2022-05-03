0528af69adb106be7cd47f1834e2171c544c9558
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-05-03 09:43:26 -0300

    Add the user field for customer model

---
+++ customer/forms.py
@@ -0,0 +1,8 @@
+# django imports
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+
+# project imports
+from .models import Customer
+from users.models import User
--- customer/models.py
+++ customer/models.py
@@ -1,14 +1,19 @@
 from django.db import models
-from django.forms import CharField, DateField, DateTimeField
 
 from users.models import User
 
 
 class Customer(models.Model):
+    GENDER_OPTIONS = (
+        ('m', 'Masculino'),
+        ('f', 'Feminino'),
+        ('o', 'Outros'),
+    )
+    
     name = models.CharField(max_length=150)
     cpf = models.CharField(max_length=11)
     birth_date = models.DateField()
-    gender = models.CharField(max_length=1)
+    gender = models.CharField(max_length=1, choices=GENDER_OPTIONS)
     postal_code = models.CharField(max_length=8)
     address = models.CharField(max_length=150)
     address_number = models.CharField(max_length=7)
@@ -19,7 +24,12 @@ class Customer(models.Model):
     tel1 = models.CharField(max_length=11)
     tel2 = models.CharField(max_length=11, null=True)
     active = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='customer')
     creator = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='customer_creator_set')
     updater = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name='customer_updater_set')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
