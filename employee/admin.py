200fac789271eaa4757f06409587b14edf363957
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-05-06 18:24:02 -0300

    Add employee model in the admin panel

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Employee
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'cpf',
+        'birth_date',
+        'gender',
+        'crm',
+        'specialty',
+        'postal_code',
+        'address',
+        'address_number',
+        'complement',
+        'state',
+        'city',
+        'neighborhood',
+        'tel1',
+        'tel2',
+        'active',
+        'user',
+        'creator',
+        'updater',
+        'created',
+        'updated',
+    ]
--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,4 @@
+from inspect import GEN_CLOSED
 from django.db import models
 from django.forms import BooleanField
 
@@ -7,20 +8,56 @@ from specialty.models import Specialty
 
 
 class Employee(models.Model):
+    GENDER_OPTIONS = (
+        ('m', 'Masculino'),
+        ('f', 'Feminino'),
+        ('o', 'Outros'),
+    )
+
+    STATE_LIST = (
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceara'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),
+    )
+
     name = models.CharField(max_length=150)
     cpf = models.CharField(max_length=11)    
     birth_date = models.DateField()
-    gender = models.CharField(max_length=1)
-    crm = models.CharField(max_length=13, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_OPTIONS)
+    crm = models.CharField(max_length=13, blank=True, null=True)
     postal_code = models.CharField(max_length=8)
     address = models.CharField(max_length=150)
     address_number = models.CharField(max_length=7)
-    complement = models.CharField(max_length=70, null=True)
-    state = models.CharField(max_length=2)
+    complement = models.CharField(max_length=70, blank=True, null=True)
+    state = models.CharField(max_length=2, choices=STATE_LIST)
     city = models.CharField(max_length=150)
     neighborhood = models.CharField(max_length=150)
     tel1 = models.CharField(max_length=11)
-    tel2 = models.CharField(max_length=11, null=True)
+    tel2 = models.CharField(max_length=11, blank=True, null=True)
     lunch_start_time = models.TimeField()
     lunch_finish_time = models.TimeField()
     active = models.BooleanField(default=True)
@@ -30,3 +67,7 @@ class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='employee')
     creator = models.ForeignKey(User, on_delete=models.PROTECT, related_name='employee_creator_set')
     updater = models.ForeignKey(User, on_delete=models.PROTECT, related_name='employee_updater_set')
+
+
+    def __str__(self):
+        return self.name
