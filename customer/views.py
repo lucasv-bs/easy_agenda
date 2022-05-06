3e9226be5a81dff113d712d619bcb43ba11ede58
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-05-06 09:43:51 -0300

    Fix the register customer form

--- customer/views.py
+++ customer/views.py
@@ -5,8 +5,6 @@ import customer
 
 # project imports
 from .forms import CustomerCreateForm, UserCustomerCreateForm
-from .models import Customer
-from users.models import User
 
 
 def registerPage(request):
@@ -20,13 +18,9 @@ def registerPage(request):
         if form_customer.is_valid() and form_user.is_valid():
             user = form_user.save()
             
-            print(user)
-
             customer = form_customer.save(commit=False)
             customer.user = user
             customer.save()
-            
-            print(customer)
 
     context = {'form_customer': form_customer, 'form_user': form_user}
     return render(request, 'register.html', context)
