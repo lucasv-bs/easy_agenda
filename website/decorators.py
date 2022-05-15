5340d8da8e9993ed6c835697f9defe013072515f
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-05-15 17:03:20 -0300

    Add the 'admin_only' decorator to control access on pages that only admin can access

--- website/decorators.py
+++ website/decorators.py
@@ -12,3 +12,20 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
 
     return wrapper_func
+
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+        else:
+            if request.user.is_staff:
+                return redirect('employee:employee_home')
+            else:
+                return redirect('customer:customer_home')
+
+    return wrapper_func
