b75bba01dd11e850a1049228eec164a4848838c0
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-05-15 17:47:45 -0300

    Add the 'allowed_user' decorator to control access on pages by groups

--- website/decorators.py
+++ website/decorators.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import redirect
 
 
@@ -12,6 +13,24 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
 
     return wrapper_func
+
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                if request.user.is_staff:
+                    return redirect('employee:employee_home')
+                else:
+                    return redirect('customer:customer_home')
+        return wrapper_func
+    return decorator
 
 
 def admin_only(view_func):
